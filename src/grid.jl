4d36400a61c739152655fc8385ce8f0d524040f1
Leonard Jarkowski <leonard.jarkowski@gmail.com>
2025-02-04 04:46:23 +0100

    fixed neighbors for polar coordinates and fixed minima not appearing in gridpoints

--- src/grid.jl
+++ src/grid.jl
@@ -18,13 +18,6 @@ struct Cartesian2D
     polar::Number
 end
 
-function distance(t1::Spherical,t2::Spherical)
-    return sqrt(t1.radius^2 + t2.radius^2 - 2*t1.radius*t2.radius*(sin(t1.polar) * sin(t2.polar) * cos(t1.azimuth - t2. azimuth) + cos(t1.polar) * cos(t2.polar)))
-end
-
-function distance(t1::Polar,t2::Polar)
-    return sqrt(t1.radius^2 + t2.radius^2 - 2*t1.radius*t2.radius * cos(t1.polar - t2.polar))
-end
 
 
 struct Point4D{T <: Number} <: Point
@@ -57,6 +50,17 @@ struct PolarGrid <: Grid
     potential::Function
 end
 
+function distance(t1::Spherical,t2::Spherical)
+    return sqrt(t1.radius^2 + t2.radius^2 - 2*t1.radius*t2.radius*(sin(t1.polar) * sin(t2.polar) * cos(t1.azimuth - t2. azimuth) + cos(t1.polar) * cos(t2.polar)))
+end
+
+function distance(t1::Polar,t2::Polar)
+    return sqrt(t1.radius^2 + t2.radius^2 - 2*t1.radius*t2.radius * cos(t1.polar - t2.polar))
+end
+
+distance(t1::Point2D,t2::Point2D) = distance(t1.translation,t2.translation)
+
+
 function getPoints(grid::PolarGrid)
     return [Point2D(Polar(r,theta),grid.potential(r,theta)) for r in grid.rs, theta in grid.thetas]
 end
@@ -68,32 +72,33 @@ function getNeighbors(grid::PolarGrid,point::Point2D)
     radial_arg = findfirst(point.translation.radius .== grid.rs)
 
     if radial_arg == 1
-        radial_neighbors = [2]
-    elseif radial_arg == length(rs)
-        radial_neighbors = [length(rs)-1]
+        radial_neighbors = [1,2]
+    elseif radial_arg == length(grid.rs)
+        radial_neighbors = [length(grid.rs)-1,length(grid.rs)]
     else 
-        radial_neighbors = [radial_arg-1, radial_arg+1]
+        radial_neighbors = [radial_arg-1, radial_arg, radial_arg+1]
     end
 
     angular_arg = findfirst(point.translation.polar .== grid.thetas)
-    angular_neighbors = [mod1(radial_arg-1,length(grid.thetas)), mod1(radial_arg+1,length(grid.thetas))]
+    angular_neighbors = [mod1(angular_arg-1,length(grid.thetas)), angular_arg,  mod1(angular_arg+1,length(grid.thetas))]
 
-    return [Point2D(Polar(r,theta),potential(r,theta)) for r in grid.rs[radial_neighbors], theta in grid.thetas[angular_neighbors]]
+    neighbors = [Point2D(Polar(r,theta),grid.potential(r,theta)) for r in grid.rs[radial_neighbors], theta in grid.thetas[angular_neighbors]]
+    return neighbors[neighbors .!= Ref(point)]
 end
 
 
 function gradDescent(grid::Grid)
-    minima = []
-    gridpoints = Dict()
-    trajectory = []
+    minima = Point[]
+    gridpoints = Dict{Point,Tuple{Point,Point}}()
+    trajectory = Tuple{Point,Point}[]
 
     function getStep(point)
         if haskey(gridpoints,point)
-            minimum = get(gridpoints,point)
+            minimum = gridpoints[point][2]
             for step in trajectory
                 push!(gridpoints,step[1] => (step[2],minimum))
             end
-            trajectory = []
+            trajectory = Tuple{Point,Point}[]
             return 0
         end
         neighbors = getNeighbors(grid,point)
@@ -103,12 +108,13 @@ function gradDescent(grid::Grid)
         if grads[min] >= 0
             push!(minima,point)
             for step in trajectory
-                push!(gridpoints,step[1] => (step[2],minimum))
+                push!(gridpoints,step[1] => (step[2],point))
             end
-            trajectory = []
+            push!(gridpoints,point => (point,point))
+            trajectory = Tuple{Point,Point}[]
         else
             push!(trajectory,(point, neighbors[min]))
-            getStep(neighbors(min))
+            getStep(neighbors[min])
         end
     end
 
