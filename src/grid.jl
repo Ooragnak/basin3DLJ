538dc0d2684a827d5e28c406fd5ea021e28654df
Leonard Jarkowski <leonard.jarkowski@gmail.com>
2025-02-14 17:55:00 +0100

    deprecated PolarGrid struct and related functions

--- src/grid.jl
+++ src/grid.jl
@@ -59,12 +59,6 @@ struct PointGrid{T <: Point} <: Grid{T}
     properties::AbstractString
 end
 
-struct PolarGrid{T <: Point} <: Grid{T}
-    dim::Float64
-    rs::Vector{Float64}
-    thetas::Vector{Float64}
-    potential::Function
-end
 
 """
 Data structure encoding the basins of attraction \n
@@ -102,31 +96,6 @@ function getNeighbors(grid::PointGrid,point::Point)
     return grid.points[first.(g)], last.(g)
 end
 
-function getPoints(grid::PolarGrid)
-    return [Point2D(Polar(r,theta),grid.potential(r,theta)) for r in grid.rs, theta in grid.thetas]
-end
-
-function getNeighbors(grid::PolarGrid,point::Point)
-    @assert point.translation.radius in grid.rs "Provided point not in given radial steps"
-    @assert point.translation.polar in grid.thetas "Provided angle (polar) not in given angular steps"
-
-    radial_arg = findfirst(point.translation.radius .== grid.rs)
-
-    if radial_arg == 1
-        radial_neighbors = [1,2]
-    elseif radial_arg == length(grid.rs)
-        radial_neighbors = [length(grid.rs)-1,length(grid.rs)]
-    else 
-        radial_neighbors = [radial_arg-1, radial_arg, radial_arg+1]
-    end
-
-    angular_arg = findfirst(point.translation.polar .== grid.thetas)
-    angular_neighbors = [mod1(angular_arg-1,length(grid.thetas)), angular_arg,  mod1(angular_arg+1,length(grid.thetas))]
-
-    neighbors = [Point2D(Polar(r,theta),grid.potential(r,theta)) for r in grid.rs[radial_neighbors], theta in grid.thetas[angular_neighbors]]
-    withoutSelf = findall(x -> x != point,vec(neighbors))
-    return neighbors[withoutSelf], [distance(n,point) for n in neighbors][withoutSelf]
-end
 
 function findClosestGridPoint(grid::PointGrid,point::Point)
     @assert eltype(grid.points) == typeof(point)
