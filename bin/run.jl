fe07fd08530b83a953930dbb56967ce78d843ab8
Leonard Jarkowski <leonard.jarkowski@gmail.com>
2025-02-19 05:09:28 +0100

    initialized

---
+++ bin/run.jl
@@ -0,0 +1,7 @@
+include("../src/grid.jl")
+include("../src/potential.jl")
+include("../src/plots.jl")
+include("../src/theme.jl")
+
+################################################################
+
