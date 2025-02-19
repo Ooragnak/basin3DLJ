a2071241c3397e358e5945122b6ccd1d8b6ac3f4
Leonard Jarkowski <leonard.jarkowski@gmail.com>
2025-02-19 05:09:17 +0100

    consolildated conversions

--- src/plots.jl
+++ src/plots.jl
@@ -1,35 +1,23 @@
-include(joinpath(rootPath, "src/theme.jl"))
+include("../src/theme.jl")
 
 ################################################################
 
 # Array based conversions
 
-Makie.convert_arguments(P::GridBased, ps::AbstractVector{<: Point}) = 
+Makie.convert_arguments(P::T, ps::AbstractVector{<: Point}) where {T <: Union{PointBased, GridBased}} = 
     convert_arguments(P, [p.translation for p in ps], [p.energy for p in ps])
 
-Makie.convert_arguments(P::T, ps::AbstractVector{<: Point}) where {T <: Union{PointBased, GridBased}}= 
-    convert_arguments(P, [p.translation for p in ps], [p.energy for p in ps])
-
-Makie.convert_arguments(P::PointBased, ts::AbstractVector{Polar}, args...) =
+Makie.convert_arguments(P::T, ts::AbstractVector{Polar}, args...) where {T <: Union{PointBased, GridBased}}= 
     convert_arguments(P, [t.θ for t in ts], [t.r for t in ts], args...)
 
-Makie.convert_arguments(P::PointBased, ts::AbstractVector{Cartesian2D}, args...) =
+Makie.convert_arguments(P::T, ts::AbstractVector{Cartesian2D}, args...) where {T <: Union{PointBased, GridBased}}= 
     convert_arguments(P, [t.x for t in ts], [t.y for t in ts], args...)
 
-Makie.convert_arguments(P::PointBased, ts::AbstractVector{Cartesian3D}, args...) =
+Makie.convert_arguments(P::T, ts::AbstractVector{Cartesian3D}, args...) where {T <: Union{PointBased, GridBased}}= 
     convert_arguments(P, [t.x for t in ts], [t.y for t in ts], [t.z for t in ts], args...)
 
 Makie.convert_arguments(P::Type{<:Scatter}, ps::AbstractVector{<: Point}) = 
     convert_arguments(P, [p.translation for p in ps])
-
-Makie.convert_arguments(P::GridBased, ts::AbstractVector{Cartesian2D}, args...) =
-    convert_arguments(P, [t.x for t in ts], [t.y for t in ts], args...)
-
-Makie.convert_arguments(P::GridBased, ts::AbstractVector{Cartesian3D}, args...) =
-    convert_arguments(P, [t.x for t in ts], [t.y for t in ts],[t.z for t in ts], args...)
-
-Makie.convert_arguments(P::GridBased, ts::AbstractVector{Polar}, args...) =
-    convert_arguments(P, [t.θ for t in ts], [t.r for t in ts], args...)
 
 Makie.convert_arguments(P::Type{<:Surface}, ts::AbstractVector{Cartesian2D}, args...) =
     convert_arguments(P, [t.x for t in ts], [t.y for t in ts], args...)
