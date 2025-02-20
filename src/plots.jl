c97fe187b5f22992f51a5b768c3d5800317138a4
Leonard Jarkowski <leonard.jarkowski@gmail.com>
2025-02-20 22:30:34 +0100

    moved interpolation functions

--- src/plots.jl
+++ src/plots.jl
@@ -1,5 +1,8 @@
 include("../src/theme.jl")
 
+using KernelAbstractions
+using AMDGPU
+using CUDA
 ################################################################
 
 # Array based conversions
@@ -38,3 +41,68 @@ Makie.convert_arguments(P::PointBased, t::Cartesian3D, args...) =
 
 Makie.convert_arguments(P::Type{<:Scatter}, p::Point, args...) =
     convert_arguments(P, p.translation, args...)
+
+function interpolateSlice(grid::PointGrid{<: Point{T}},xs,ys,zs ;power=8,ArrayType = Array,closest=false) where {T <: Position}
+    N = length(xs)*length(ys)*length(zs)
+    xarr = ArrayType([x for x in xs, y in ys, z in zs])
+    yarr = ArrayType([y for x in xs, y in ys, z in zs])
+    zarr = ArrayType([z for x in xs, y in ys, z in zs])
+
+    pxarr = ArrayType([p.translation.x for p in grid.points])
+    pyarr = ArrayType([p.translation.y for p in grid.points])
+    pzarr = ArrayType([p.translation.z for p in grid.points])
+
+    energyarr = ArrayType([p.energy for p in grid.points])
+    energies = ArrayType(zeros(eltype(xarr),size(xarr)))
+
+    interpolateScalar!(energies, pxarr, pyarr, pzarr, energyarr, xarr, yarr, zarr; power= power, closest= closest)
+
+    return reshape(energies,(length(xs),length(ys),length(zs)))
+end
+
+function interpolateScalar!(scalar::T, pxs::K, pys::K, pzs::K, pscalar::K, xs::T, ys::T, zs::T; power=12, closest=false) where {T <: AbstractArray, K <: AbstractArray}
+    @assert length(xs) == length(ys) == length(zs)
+    @assert length(pxs) == length(pys) == length(pzs) == length(pscalar)
+
+    N = length(xs)
+    M = length(pxs)
+
+    backend = get_backend(pxs)
+    if !closest 
+        kernel! = interpolatePoint!(backend,256)
+        kernel!(scalar,xs,ys,zs,pscalar,pxs,pys,pzs,M,power;ndrange = N)
+    else
+        kernel! = closestPoint!(backend,256)
+        kernel!(scalar,xs,ys,zs,pscalar,pxs,pys,pzs,M;ndrange = N)
+    end
+end
+
+@kernel function interpolatePoint!(out,@Const(xvals),@Const(yvals),@Const(zvals),@Const(scalar),@Const(px),@Const(py),@Const(pz),n,power)
+    j = @index(Global, Linear)
+    weights = 0.0
+    energy = 0.0
+    weight = 0.0
+    for i in 1:n
+        weight = (1 / sqrt((xvals[j] - px[i])^2 + (yvals[j] - py[i])^2 + (zvals[j] - pz[i])^2))^power
+        weights += weight
+        energy += scalar[i] * weight
+    end
+    out[j] = energy / weights
+end
+
+
+
+@kernel function closestPoint!(out,@Const(xvals),@Const(yvals),@Const(zvals),@Const(scalar),@Const(px),@Const(py),@Const(pz),n)
+    j = @index(Global, Linear)
+    smallestDistance = Inf
+    distance = 0.0
+    energy = 0.0
+    for i in 1:n
+        distance = (xvals[j] - px[i])^2 + (yvals[j] - py[i])^2 + (zvals[j] - pz[i])^2
+        if distance < smallestDistance
+            smallestDistance = distance
+            energy = scalar[i]
+        end
+    end
+    out[j] = energy
+end
