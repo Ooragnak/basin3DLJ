8940ceee0135c37aed625735dfb18ae769d5b6b0
Leonard Jarkowski <leonard.jarkowski@gmail.com>
2025-02-21 17:21:55 +0100

    added isosurface plotting

--- src/plots.jl
+++ src/plots.jl
@@ -42,7 +42,7 @@ Makie.convert_arguments(P::PointBased, t::Cartesian3D, args...) =
 Makie.convert_arguments(P::Type{<:Scatter}, p::Point, args...) =
     convert_arguments(P, p.translation, args...)
 
-function interpolateSlice(grid::PointGrid{<: Point{T}},xs,ys,zs ;power=8,ArrayType = Array,closest=false) where {T <: Position}
+function interpolateSlice(grid::PointGrid{<: Point{T}},xs,ys,zs ;power=8,ArrayType = Array,closest=false,getPoints=false) where {T <: Position}
     N = length(xs)*length(ys)*length(zs)
     xarr  = ArrayType([x for x in xs, y in ys, z in zs])
     yarr  = ArrayType([y for x in xs, y in ys, z in zs])
@@ -52,14 +52,19 @@ function interpolateSlice(grid::PointGrid{<: Point{T}},xs,ys,zs ;power=8,ArrayTy
     pzarr = ArrayType([p.translation.z for p in grid.points])
 
     energyarr = ArrayType([p.energy for p in grid.points])
-    energies = ArrayType(zeros(eltype(xarr),size(xarr)))
-
-    interpolateScalar!(energies, pxarr, pyarr, pzarr, energyarr, xarr, yarr, zarr; power= power, closest= closest)
-
-    return reshape(energies,(length(xs),length(ys),length(zs)))
+    if !getPoints
+        energies = ArrayType(zeros(eltype(xarr),size(xarr)))
+        interpolateScalar!(energies, pxarr, pyarr, pzarr, energyarr, xarr, yarr, zarr; power= power, closest= closest)
+        return reshape(energies,(length(xs),length(ys),length(zs)))
+    else
+        pIndices = Int32.(ArrayType(zeros(eltype(xarr),size(xarr))))
+        interpolateScalar!(pIndices, pxarr, pyarr, pzarr, energyarr, xarr, yarr, zarr; power= power, closest= closest,getIndex=true)
+        return [grid.points[i] for i in Array(pIndices)]
+    end
 end
 
-function interpolateScalar!(scalar::T, pxs::K, pys::K, pzs::K, pscalar::K, xs::T, ys::T, zs::T; power=12, closest=false) where {T <: AbstractArray, K <: AbstractArray}
+
+function interpolateScalar!(scalar::U, pxs::K, pys::K, pzs::K, pscalar::K, xs::T, ys::T, zs::T; power=12, closest=false,getIndex=false) where {U <: AbstractArray, T <: AbstractArray, K <: AbstractArray}
     @assert length(xs) == length(ys) == length(zs)
     @assert length(pxs) == length(pys) == length(pzs) == length(pscalar)
 
@@ -70,6 +75,9 @@ function interpolateScalar!(scalar::T, pxs::K, pys::K, pzs::K, pscalar::K, xs::T
     if !closest 
         kernel! = interpolatePoint!(backend,256)
         kernel!(scalar,xs,ys,zs,pscalar,pxs,pys,pzs,M,power;ndrange = N)
+    elseif getIndex
+        kernel! = closestPointIndex!(backend,256)
+        kernel!(scalar,xs,ys,zs,pscalar,pxs,pys,pzs,M;ndrange = N)
     else
         kernel! = closestPoint!(backend,256)
         kernel!(scalar,xs,ys,zs,pscalar,pxs,pys,pzs,M;ndrange = N)
@@ -104,4 +112,70 @@ end
         end
     end
     out[j] = energy
+end
+
+@kernel function closestPointIndex!(out,@Const(xvals),@Const(yvals),@Const(zvals),@Const(scalar),@Const(px),@Const(py),@Const(pz),n)
+    j = @index(Global, Linear)
+    smallestDistance = typemax(eltype(out))
+    distance = zero(eltype(out))
+    closestIndex = zero(eltype(out))
+    for i in 1:n
+        distance = (xvals[j] - px[i])^2 + (yvals[j] - py[i])^2 + (zvals[j] - pz[i])^2
+        if distance < smallestDistance
+            smallestDistance = distance
+            closestIndex = i
+        end
+    end
+    out[j] = closestIndex
+end
+
+function plotBasinsIsosurface(basin;interpolate=nothing,ArrayType=nothing,energyrange=nothing,figsize=(2560,1440),fontsize=40,isorange = 1,interpolationResolution = 100)
+    grid = basin.grid
+    @assert grid.dim == 3 "Grid must be 3-dimensional"
+    @assert !isnothing(interpolate) || grid.isCartesian "Missing required interpolation arguments for non-Cartesian grid."
+    @assert isnothing(interpolate) || !isnothing(interpolate) "Missing required interpolation arguments - Provide \"ArrayType\" for interpolation."
+
+    if !isnothing(interpolate)
+        xlimits = extrema(interpolate[1])
+        ylimits = extrema(interpolate[2])
+        zlimits = extrema(interpolate[3])
+        points = Array(interpolateSlice(grid,range(xlimits...,interpolationResolution),range(xlimits...,interpolationResolution),range(xlimits...,interpolationResolution),ArrayType=ArrayType,closest=true,getPoints=true))
+    else
+        xs = unique(p.translation.x for p in grid.points)
+        ys = unique(p.translation.y for p in grid.points)
+        zs = unique(p.translation.z for p in grid.points)
+        xlimits = extrema(xs)
+        ylimits = extrema(ys)
+        zlimits = extrema(zs)
+        points = reshape([p for p in grid.points],(length(xs),length(ys),length(zs)))
+    end
+
+    energies = [p.energy for p in points]
+
+    if isnothing(energyrange)
+        energyrange = (extrema(energies))
+    end
+
+    f = Figure(size=figsize, fontsize=fontsize)
+
+    s = Slider(f[1,2], range = range(energyrange...,500), startvalue = sum(energyrange)/2,horizontal=false)
+
+    isoval = lift(s.value) do z
+        z
+    end
+
+    plotTitle = lift(s.value) do z
+        latexstring(L"Basins of attraction with isosurface value $E = %$(round(z,sigdigits=3)) $")
+    end
+
+    ax = Axis3(f[1,1], title = plotTitle)
+
+    for (i,m) in enumerate(basin.minima)
+        basinPoints = [basin.gridpoints[p][2] == m ? p.energy : NaN for p in points]
+        volume!(ax,xlimits,ylimits,zlimits,basinPoints , algorithm = :iso, isovalue = isoval, isorange = isorange ,colormap = fill(Makie.wong_colors()[i],100) , interpolate = true)
+        #basinPoints = [basin.gridpoints[p][2] == m ? p.energy : NaN for p in points]
+        #isOutside = @lift x -> !(x <= $isoval)
+        #voxels!(ax,xlimits,ylimits,zlimits,basinPoints, colormap = fill(Makie.wong_colors()[i],100),is_air = isOutside)
+    end
+    return f
 end
